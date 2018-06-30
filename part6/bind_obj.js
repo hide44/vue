33d6d69027c7dc5c197ad82882c83481f116688c
hide44 <ryo_081115@yahoo.co.jp>
2018-07-01 03:54:17 +0900

    finished part6

---
+++ part6/bind_obj.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#app',
+	data: {
+		attrs: {
+			size: 12,
+			maxlength: 15,
+			required: true
+		},
+		color: {
+			backgroundColor: 'Blue',
+			color: 'Red'
+		},
+		big: {
+			fontSize: '1.5em'
+		},
+		mg: 'Hello, Vue.js!'
+	}
+});
